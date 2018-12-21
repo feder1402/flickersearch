92088da1ad3c63a674a29e17446af29c8da23ed9
Federico Raggi <federicoraggi@hotmail.com>
2018-12-20 19:45:40 -0800

    Exposed all props as knobs

--- flicker-search-statechart/src/App.js
+++ flicker-search-statechart/src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
 
 import './App.css'
-const xState = {
-  appName: 'FlickrSearch (Statecharts)',
-  tag: null,
-  results: null
-}
 
-const tag = 'cats'
-
-import results from './getPhotos';
 import AppScenes from './App.Scenes';
 
-const App = ({state}) => AppScenes(state)({...xState, tag: 'cats', results})
+const App = ({state, xState}) => AppScenes(state)(xState)
 
 export default App 
--- flicker-search-statechart/stories/index.stories.js
+++ flicker-search-statechart/stories/index.stories.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { select, withKnobs } from '@storybook/addon-knobs';
+import { object, select, text, withKnobs } from '@storybook/addon-knobs';
 
 import App from '../src/App'
+import photos from '../src/getPhotos';
 
 storiesOf('Components', module)
   .addDecorator(withKnobs)
   .add('App', () => {
     const options = ['init', 'loading', 'results', 'unknown']
-    const value = select('state', options, 'init');
-    return (<App state={value} />)
+
+    const state = select('state', options, 'init');
+    const appName = text('appName', 'FlickrSearch (Statecharts)')
+    const tag = text('tag', 'cats')
+    const results = object('results', photos)
+
+    return (<App state={state} xState={{appName, tag, results}}/>)
   })
